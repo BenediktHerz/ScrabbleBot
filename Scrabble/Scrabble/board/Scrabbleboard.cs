e1151cdeb288652381eab8b76441538d48653c76
0x222222 <42623038+0x222222@users.noreply.github.com>
2019-03-22 19:58:27 +0100

    Divide into single files

---
+++ Scrabble/Scrabble/board/Scrabbleboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble
+{
+    class Scrabbleboard
+    {
+        private Array board;
+
+        public Scrabbleboard()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int k = 0; k < 15; k++)
+                {
+                    if ((i % 7 == 0 && k % 7 == 0) && !(i == 7 && k == 7))
+                    {
+                        Console.WriteLine("Im dribbel Word");
+                    }
+
+                    if ((i % 4 == 2 && k % 4 == 2) && !(1 == 0))
+                    {
+                        Console.WriteLine("Im dribbel Letter");
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Scrabble/Scrabble/dictionary/WordDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble
+{
+    class WordDictionary
+    {
+        private List<List<string>> dic = new List<List<string>>();
+
+        public WordDictionary()
+        {
+            Console.WriteLine("Im called");
+
+            JObject json = JObject.Parse(File.ReadAllText(@"words.json"));
+            JObject currentJson = json;
+            string[] dictonary;
+
+            int longest_word = 0;
+
+
+            foreach (string word in json["words"])
+            {
+                if (longest_word < word.Length)
+                {
+                    longest_word = word.Length;
+                }
+
+            }
+
+            for (int i = 1; i < longest_word; i++)
+            {
+                List<string> currentShift = new List<string>();
+
+                foreach (string word in json["words"])
+                {
+                    if (word.Length > i)
+                    {
+                        string wordWithDelemiter = word.Insert(i, ",");
+                        string[] arr = wordWithDelemiter.Split(',');
+                        string newWord = arr[1] + "," + arr[0];
+
+                        currentShift.Add(newWord);
+                    }
+
+                }
+
+
+                dic.Add(currentShift);
+            }
+        }
+
+
+        public void search(string[] pos)
+        {
+
+        }
+    }
+}
